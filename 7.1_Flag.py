d7210d31558e5df3a886b1a617a319535d21eecf
clarksongav <ClarksonGav@urbandaleschools.com>
2021-11-16 09:44:31 -0600

    my committ

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,40 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade
+SL = 494
+SH = 250
+
+BH = 140.01
+BL = 197.6
+STHOR = 14.04
+STHBT = 14.04
+STVT =16.38
+STVBT = 16.38
+STDI = 16.016
+STH = 19.994
+ZE = 19.994
+ZERO = 0
+HI = 20
+BI = 12
+arcade.open_window(SL, SH, "star wars art",True)
+arcade.set_background_color(arcade.color.RED)
+arcade.start_render()
+for i in range(7):
+    arcade.draw_line(0,ZE,SL,ZE,(255,255,255),STH)
+    ZE+=2*(STH)
+arcade.draw_lrtb_rectangle_filled(0,BL,SH,SH-BH+1,arcade.color.BLUE)
+for x in range(5):
+    for i in range(6):
+        arcade.draw_text("*", ZERO+16.38, SH-STVT-HI, arcade.color.WHITE, 25)
+        ZERO+=30
+    HI+=2*STVBT-5
+    ZERO = 0
+for i in range(4):
+    for i in range(5):
+        arcade.draw_text("*", ZERO+2*STVT, SH-2*STVBT-BI-5, arcade.color.WHITE, 25)
+        ZERO+=30
+    BI += 2 * STVBT - 5
+    ZERO = 0
+arcade.finish_render()
+arcade.run()
--- 7.2_Pycasso.py
+++ 7.2_Pycasso.py
@@ -14,7 +14,38 @@ Then add a comment at the top telling the reader what you are drawing.
 IN THE WINDOW TITLE PLEASE PUT YOUR NAME.
 When you are finished Pull Request your file to your instructor.
 '''
+import arcade
 
-
-
+arcade.open_window(500, 700, "Gavin Clarkson",True, True)
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+arcade.draw_ellipse_filled(120,400,153,1003, arcade.color.BLACK, 355)
+arcade.draw_ellipse_filled(380,400,153,1003, arcade.color.BLACK, 5)
+arcade.draw_ellipse_filled(380,400,150,1000, arcade.color.YELLOW, 5)
+arcade.draw_ellipse_filled(120,400,150,1000, arcade.color.YELLOW, 355)
+arcade.draw_circle_filled(250,-600,1002, arcade.color.BLACK)
+arcade.draw_ellipse_filled(380,400,100,666, arcade.color.YELLOW, 5)
+arcade.draw_ellipse_filled(120,400,100,666, arcade.color.YELLOW, 355)
+arcade.draw_circle_filled(250,-600,1000, arcade.color.YELLOW)
+arcade.draw_circle_filled(0,50,77, arcade.color.BLACK)
+arcade.draw_circle_filled(500,50,77, arcade.color.BLACK)
+arcade.draw_circle_filled(0,50,75, arcade.color.RED)
+arcade.draw_circle_filled(500,50,75, arcade.color.RED)
+arcade.draw_circle_filled(100,200,60,arcade.color.BLACK)
+arcade.draw_circle_filled(400,200,60,arcade.color.BLACK)
+arcade.draw_circle_filled(115,215,30,arcade.color.WHITE)
+arcade.draw_circle_filled(385,215,30,arcade.color.WHITE)
+arcade.draw_ellipse_filled(250,175,30,10,arcade.color.BLACK)
+arcade.draw_arc_filled(200,100,100,50,arcade.color.BLACK,190, 350)
+arcade.draw_arc_filled(300,100,100,50,arcade.color.BLACK,190, 350)
+arcade.draw_arc_filled(200,102,100,50,arcade.color.YELLOW,190, 350)
+arcade.draw_arc_filled(300,102,100,50,arcade.color.YELLOW,190, 350)
+arcade.draw_circle_filled(250,450, 150, arcade.color.BLACK)
+arcade.draw_arc_filled(250,450,280,285,arcade.color.RED,0,180)
+arcade.draw_arc_filled(250,450,280,285,arcade.color.WHITE,180,360)
+arcade.draw_rectangle_filled(250,450,280,15,arcade.color.BLACK)
+arcade.draw_circle_filled(250,450,45,arcade.color.BLACK)
+arcade.draw_circle_filled(250,450,32,arcade.color.WHITE)
+arcade.finish_render()
+arcade.run()
 
